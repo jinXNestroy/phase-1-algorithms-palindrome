037b1d20ef2c9b2830350ce83473e65a257bf7c3
DanielJoel <danpersie53@gmail.com>
2024-04-15 22:29:44 +0300

    Algorithims lab

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let wordArray = word.split('')
+  let output
+
+  // Reverse wordArrat
+  let reversedWordArray = []
+  for (let i = wordArray.length - 1; i >= 0; i--) {
+    reversedWordArray.push(wordArray[i])
+  }
+
+  // Compare each index
+
+  for (let index in wordArray) {
+    if (wordArray[index] === reversedWordArray[index]) {
+      return true;
+    }
+    else {
+
+      return false;
+    }
+  }
+
+
 }
 
-/* 
+
+
+
+/*
   Add your pseudocode here
+  Convert whatever is input as the arg to an array
+  create a new array that is reversed
+  compare each index in the reversed array to the original one
+  if every index matches output true if it doesn't false
 */
 
 /*
